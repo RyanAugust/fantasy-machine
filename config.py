57e7a2b3c53e94b2f17d7e30ec969d4d1a1239be
RyanAugust <rad19206@yahoo.com>
2019-03-10 13:24:25 -0400

    new location of event db

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 user = os.environ['USER']
 
 # PATHS
-mlb_data_path = os.getcwd() + "/data/MLB_eventfiles"
-mlb_db_path = os.getcwd() + "/data/MLB.db"
+mlb_data_path = os.getcwd() + "/data/baseball_data"
+mlb_db_path = os.getcwd() + "/data/bevent.db"
