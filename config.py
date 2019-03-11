7b47b945c6a9fad8ca815ab1de486fa6944f6a12
RyanAugust <rad19206@yahoo.com>
2019-03-11 11:12:11 -0400

    New DB location and table for lineups

--- config.py
+++ config.py
@@ -4,3 +4,8 @@ user = os.environ['USER']
 # PATHS
 mlb_data_path = os.getcwd() + "/data/baseball_data"
 mlb_db_path = os.getcwd() + "/data/bevent.db"
+
+
+## Lineups
+lineups_db_path = os.getcwd() + "/data/lineups.db"
+lineups_table = 'daily_lineups'
