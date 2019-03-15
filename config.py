e4d4d6ead315396bf0a0f1bee9624cdf17d6f1e5
RyanAugust <rad19206@yahoo.com>
2019-03-15 11:02:44 -0400

    updated config mapping for player id map

--- config.py
+++ config.py
@@ -2,10 +2,14 @@ import os
 user = os.environ['USER']
 
 # PATHS
+## Game/Event Data
 mlb_data_path = os.getcwd() + "/data/baseball_data"
 mlb_db_path = os.getcwd() + "/data/bevent.db"
-
 
 ## Lineups
 lineups_db_path = os.getcwd() + "/data/lineups.db"
 lineups_table = 'daily_lineups'
+
+## ID mapping
+player_mapping_db = os.getcwd() + "/data/playerid.db"
+player_map_table = 'playerid_map'
