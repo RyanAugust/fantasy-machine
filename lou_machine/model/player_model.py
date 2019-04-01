9fd86b5c760d0f7137d2cb4e268b0bb0430eaecb
RyanAugust <rad19206@yahoo.com>
2019-03-31 23:24:09 -0400

    player model holder file

---
+++ lou_machine/model/player_model.py
@@ -0,0 +1,9 @@
+"""
+Input: fixtures and their lineups
+Output: player projections for those games.
+	* Contains a projected line of stats
+	* standard deviation of those projections
+	ie. troum001 |   H  |  BB  |  1B  |  2B  |  3B  |  HR  |
+		---------------------------------------------------
+	 		proj | 1.23 | 0.76 | 0.88 | 0.27 | 0.18 | 0.11 |
+	 		 sd  | 0.28 | 0.14 | 0.34 | 0.11 | 0.12 | 0.15 |
