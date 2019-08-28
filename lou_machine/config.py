7c47cc2e8dc1f5db12f0a13302f0fc65ff71ae81
RyanAugust <rad19206@yahoo.com>
2019-08-28 13:00:30 -0400

    new logic for distinguishing locations

--- lou_machine/config.py
+++ lou_machine/config.py
@@ -2,26 +2,27 @@
 # user = os.environ['USER']
 # lou_machine_dir = '/'.join(os.getcwd().split('/')[:-1])
 # lou_machine_dir = os.getcwd()
+base_path = '.'
 
 # PATHS
 ## Temp dirs
-temp_data_dir = "./data/temp"
+temp_data_dir = "{base_path}/data/temp".format(base_path=base_path)
 
 ## Game/Event Data
-mlb_data_path = "./data/baseball_data"
-mlb_db_path = "./data/bevent.db"
+mlb_data_path = "{base_path}/data/baseball_data".format(base_path=base_path)
+mlb_db_path = "{base_path}/data/bevent.db".format(base_path=base_path)
 mlb_event_table = 'bevent'
 
 ## Players
-players_db_path = "./data/players.db"
+players_db_path = "{base_path}/data/players.db".format(base_path=base_path)
 lineups_table = 'daily_lineups'
 roster_table = 'team_rosters'
 depthchart_table = 'depth_charts'
 
 ## ID mapping
-id_mapping_db = "./data/id_mapping.db"
+id_mapping_db = "{base_path}/data/id_mapping.db".format(base_path=base_path)
 player_map_table = 'playerid_map'
 team_abr_table = 'teamabr_map'
 
 ## Fantasy Scoring Matrix
-f_scoring = "./data/fscoring.xlsx"
+f_scoring = "{base_path}/data/fscoring.xlsx".format(base_path=base_path)
