813e6ff2cc670c55333aa595e56382fbd525d05c
RyanAugust <rad19206@yahoo.com>
2019-09-11 18:13:15 -0400

    alter naming and methods for stats generation. Objective is to generalize for application to multiple sports

--- fantasy_machine/__init__.py
+++ fantasy_machine/__init__.py
@@ -1,5 +1,5 @@
 from .update_data import update_data, import_data
-from .stats import baseball_stats
+from .stats import stats
 from fantasy_machine import model
 from fantasy_machine import config
 
